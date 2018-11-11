295ce714b619a19e12daf02d000570801439306c
alexv <alexvig796@outlook.com>
2018-11-11 02:51:10 +0200

    Forgot this one behind.

---
+++ InternetShop/Models/ProductsPageModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InternetShop.Models
+{
+    public class ProductsPageModel
+    {
+        public IEnumerable<Product> Products
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<CategoryResult> Categories
+        {
+            get;
+            set;
+        }
+
+        public ProductsPageModel()
+        {
+        }
+    }
+}
